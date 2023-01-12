abf1f65b6989c1f3cbf665600f39ed9bd227cd89
Mikhail Bobrov <BOBRTVLOL@gmail.com>
2023-01-12 17:58:27 +0100

    Course day 30: finished student project.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -11,8 +12,11 @@ def home():
 
 @app.route("/api/v1/<word>")
 def give_definition(word):
-    return {"word": word,
-            "definition": word.upper()}
+    df = pd.read_csv("dictionary.csv")
+    definition = df[df["word"] == word]["definition"].item()
+
+    return {"definition": definition,
+            "word": word}
 
 
 if __name__ == "__main__":
